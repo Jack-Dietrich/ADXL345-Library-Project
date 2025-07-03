6b388197d20035505feb741754c764ad4d2e7aea
Jack Dietrich <jed123@rogers.com>
2025-07-03 16:19:29 -0400

    Writing proof of concept +-Need to write to the device in order to trigger it to send back info. 1st bit is write/read write is 0, read is 1? 2nd bit is MB if we want to read multiple bytes
    -Need to write to the device in order to trigger it to send back info.

--- src/main.cpp
+++ src/main.cpp
@@ -8,34 +8,66 @@ MISO: 19
 SCK: 18
 SS:5
 
+--Logic Analyzer channel info--
+ch0 - SS
+ch1 -miso/sdo
+ch2 - mosi/sda
+ch3 - scl
+
 */
 
-#define ss 10 //slave select on pin 10
+//defining hspi and vspi
+
+#define VSPI 3
+
+
+//pin delcarations
+
+const int VSPI_MISO = 19;
+const int VSPI_MOSI = 23;
+const int VSPI_SCLK = 18;
+const int VSPI_SS = 5;
+
 
 // put function declarations here:
 
-int adxl345_init(){
-
-  /*
-  CS most go low at start of transmission, high at end of transmission
-  MODE should be 3 as both CPOL and CPHA are 1
-  */
-  Serial.begin(115200);
-  pinMode(ss,OUTPUT);
-  SPI.begin();
-  SPI.setBitOrder(MSBFIRST);//unsure if this is the correct way, unable to find in documentation so far...
-  SPI.setDataMode(3); //mode 3 as CPOL and CPHA 1
-  digitalWrite(ss,LOW);//START transmission
-  SPI.transfer(0X31); //want to clear bit in DATA_FORMAT register in order to select 4 wire SPI
-  
-
-  digitalWrite(ss,HIGH);//end transmission
-}
+SPIClass vspi = SPIClass(VSPI);
 
 
 void setup() {
-  adxl345_init();
+  /*
+  Notes:
+
+  */
+
+  vspi.begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_SS);
+  pinMode(VSPI_SS,OUTPUT);
+  digitalWrite(VSPI_SS,HIGH);//slave selct should be active lo, so go high asap
+
+  Serial.begin(115200);
+
+  vspi.setDataMode(3); //mode 3 as CPOL and CPHA 1.
+  digitalWrite(VSPI_SS,LOW);//START transmission
+
+  //read from register 31
+  vspi.transfer(0xB1);//read register 31(MSB is a 1)
+
+  byte buff;
+
+  int num = 1;//for testing
   
+  buff = vspi.transfer(0x00);//transfer 0 in order to prompt for data?
+
+
+
+
+
+  //manipulate to clear bit d6(xor with 1 in bit d6)
+  buff ^= (1 << 6);
+  //write back to register 0x31
+  vspi.transfer(buff); //want to clear bit in DATA_FORMAT register in order to select 4 wire SPI, first 2 bits can be 0 as writing only one byte
+
+  digitalWrite(VSPI_SS,HIGH);//end transmission
 }
 
 void loop() {
